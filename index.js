9a6db8415bb89e9c036e5c56d427601d657ba243
Wu-MengChe <black.eagle.man@gmail.com>
2023-01-06 00:11:04 +0800

    [修正] 修正曲線錯誤，以及添加顏色

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 let canvas = document.querySelector('.canvas');
 let context = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 context.strokeStyle = "rgba(52, 18, 14)";
 context.lineWidth = 1;
-for (let i = 0; i < 1000; i++) {
-	let x = i * window.innerWidth;
-	let y = Math.sin(x / window.innerWidth) * 20 + 100;
+let mountainPos = {x: [], y: []};
+for (let i = 0; i < 101; i++) {
+	mountainPos.x[i] = i * window.innerWidth / 100;
+	mountainPos.y[i] = Math.sin((mountainPos.x[i] / window.innerWidth) * Math.PI / 2) * 200 + window.innerHeight - 300;
 	if (i == 0) {
-		context.moveTo(x, y);
+		context.moveTo(mountainPos.x[i], mountainPos.y[i]);
 	} else {
-		context.lineTo(x, y);
+		context.lineTo(mountainPos.x[i], mountainPos.y[i]);
 	}
 }
+context.lineTo(window.innerWidth, window.innerHeight);
+context.lineTo(0, window.innerHeight);
 context.closePath();
+context.fillStyle = "rgb(0, 200, 0)";
 context.stroke();
+context.fill();
